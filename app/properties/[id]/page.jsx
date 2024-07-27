0951b1f2025ea097dc39b8176d21697d298493da
satyajitgiram <satyajitgiram2000@gmail.com>
2024-07-27 19:51:22 +0530

    unserstand client components & server components

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,19 @@
+'use client';
+
+import { useParams, usePathname, useRouter, useSearchParams } from "next/navigation";
+
+
 const PropertyPage = () => {
+    const router = useRouter();
+    const {id} = useParams();
+    const searchParams = useSearchParams();
+    const name = searchParams.get('name')
+    const pathname = usePathname();
+
+
   return (
-    <div>Single PropertyPage</div>
+    <div>Single PropertyPage <br></br> <button onClick={()=>router.push('/')} className="bg-blue-500 p-2" >Go To Home {pathname}</button></div>
+    
   )
 }
 
