a1c3cf9898b9f06a5a48bf3d50777513f22109e8
satyajitgiram <satyajitgiram2000@gmail.com>
2024-07-27 15:58:23 +0530

    created project and added routes

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const HomePage = () => {
+  return (
+    <div>
+    <h1 className='text-3xl'>Welcome</h1>
+    <Link href="/properties">Go to properties</Link>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ app/properties/page.jsx
@@ -0,0 +1,7 @@
+const PropertiesPage = () => {
+  return (
+    <div>PropertiesPage</div>
+  )
+}
+
+export default PropertiesPage
